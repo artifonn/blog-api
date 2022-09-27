f78f9e2c137ac9656e164c759a4b64896a1af2c8
Artifon <serginhoartifon@gmail.com>
2022-09-27 18:06:58 -0300

    Implementa o model PostCategory

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const PostCategorie = sequelize.difine('PostCategory',
+  const PostCategory = sequelize.define('PostCategory',
     {
       postId: {
         type: DataTypes.INTEGER,
@@ -16,4 +16,23 @@ module.exports = (sequelize, DataTypes) => {
       tableName: 'posts_categories',
     }
   );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'books',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost,
+      {
+        as: 'blog_posts',
+        through: PostCategory,
+        foreignKey: 'categoryId',
+        otherKey: 'postId',
+      }
+    );
+  };
+  return PostCategory;
 }
