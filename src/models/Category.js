e0b9aa5f063c23fe4a3b342be7a728d5e8362b9b
Artifon <serginhoartifon@gmail.com>
2022-09-27 15:36:36 -0300

    Implementa o model User

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+
+module.exports = (sequelize, DataType) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataType.INTEGER
+    },
+    name: DataType.STRING,
+  },
+    {
+      timestamps: false,
+      underscored: true,
+    }
+  );
+
+  return Category;
+}
